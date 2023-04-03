aefa59bdb838bcdcfc55a6fb19a8fba994ad7b16
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-04-03 19:07:36 -0300

    Pagina de inicio con React

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <a href="" className="logo">IA-Blog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
       </header>
-    </div>
+        <div className="post">
+          <div className="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=730&crop=1" alt="" />
+          </div>
+          <div className="texts">
+          <h2>Nuevos modelos de robots delivery</h2>
+          <p className="info">
+            <a className="author">Santiago Quiroz</a>
+            <time>2023-04-06 15:18</time>
+          </p>
+          <p className="sumary">Today at its special launch event, home backup power giant EcoFlow launddched a flurry of ew products, including a “Whole-Home Backup Power Solution.”</p>
+          </div>
+        </div>
+        <div className="post">
+          <div className="image">            
+          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=730&crop=1" alt="" />
+          </div>
+          <div className="texts">
+          <h2>Nuevos modelos de robots delivery</h2>
+          <p className="info">
+            <a className="author">Santiago Quiroz</a>
+            <time>2023-04-06 15:18</time>
+          </p>
+          <p className="sumary">Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
+          </div>
+        </div>
+        <div className="post">
+          <div className="image">  
+          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=730&crop=1" alt="" />
+          </div>
+          <div className="texts">
+          <h2>Nuevos modelos de robots delivery</h2>
+          <p className="info">
+            <a className="author">Santiago Quiroz</a>
+            <time>2023-04-06 15:18</time>
+          </p>
+          <p className="sumary">Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
+          </div>
+        </div>
+    </main>
   );
 }
 
