bffdd428fe407becc145f9d0c98d8a167fdab045
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-04-08 15:44:02 -0300

    Routing con react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
 import "./App.css";
 import Header from "./Header";
+import Layout from "./Layout";
 import Post from "./Post";
 import { Route, Routes } from "react-router-dom";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     /* Routes */
     <Routes>
-      <Route index element={
-        <main>
-            {/* Components */}
-            <Header />
-            <Post />
-            <Post />
-            <Post />
-          </main>
-        }/>
-        <Route path={'/login'} element={
-          <div>login</div>
-        } />
+      <Route path="/" element={<Layout />}>
+      <Route index element={<IndexPage />} />
+        <Route path="/login" element={ <LoginPage /> } />
+      </Route>
     </Routes>
 
   );
--- client/src/Header.js
+++ client/src/Header.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header>
-      <a href="" className="logo">
+      <Link to="/" className="logo">
         IA-Blog
-      </a>
+      </Link>
       <nav>
         <Link to='/login'>Login</Link>
         <Link to="/register">Register</Link>
