c1fed07dac425e82502664d7840d7f39b4562b75
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-11-25 22:15:39 +0000

    Backend para autenticacion

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,10 +1,33 @@
+import { useState } from 'react';
 
 const RegisterPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function register(ev) {
+        ev.preventDefault();
+        await fetch('http://localhost:4000/register', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        })
+    }
+
     return (
-        <form className='register' action="">
+        <form className='register' onSubmit={register}>
             <h1>Register</h1>
-            <input type="text" placeholder="Nombre de usuario" />
-            <input type="password" placeholder="Contraseña" />
+            <input
+                type="text"
+                placeholder="Nombre de usuario"
+                value={username}
+                onChange={ev => setUsername(ev.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Contraseña"
+                value={password}
+                onChange={ev => setPassword(ev.target.value)}
+            />
             <button>Register</button>
         </form>
     )
