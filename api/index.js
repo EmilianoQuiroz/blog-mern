06ea8bc8dd7c467d3903b4588665700181fc092a
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-11-26 13:33:18 +0000

    Login and Register Authentication

--- api/index.js
+++ api/index.js
@@ -4,10 +4,12 @@ const { default: mongoose } = require('mongoose');
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const app = express();
+const jwt = require('jsonwebtoken');
 
 const salt = bcrypt.genSaltSync(10);
+const secret = 'jkjasdknkjnasdndoainkllsoijlanfkns';
 
-app.use(cors());
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://santiagoquirozdev:dNGo2RfuoYRP2Np4@cluster0.v0dd31q.mongodb.net/?retryWrites=true&w=majority');
@@ -26,10 +28,19 @@ app.post('/register', async (req, res) => {
 
 });
 
-app.post('/login', async(req, res) => {
+app.post('/login', async (req, res) => {
     const { username, password } = req.body;
-        const userDoc = await User.findOne({username});
-
+    const userDoc = await User.findOne({ username });
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+        //Logged In
+        jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).json('OK');
+        });
+    } else {
+        res.status(400).json('Mail o contraseña incorrectos');
+    }
 });
 
 app.listen(4000);
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,19 +1,30 @@
 import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
 
 const LoginPage = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [redirect, setRedirect] = useState(false);
 
     async function login(ev) {
         ev.preventDefault();
-        await fetch('http://localhost:4000/login', {
+        const response = await fetch('http://localhost:4000/login', {
             method: 'POST',
             body: JSON.stringify({ username, password }),
             headers: { 'Content-Type': 'application/json' },
+            credentials: 'include',
         });
+        if (response.ok) {
+            setRedirect(true);
+        } else {
+            alert('Credenciales incorrectas');
+        }
 
     }
 
+    if (redirect) {
+        return <Navigate to={'/'} />
+    }
     return (
         <form className="login" onSubmit={login}>
             <h1>Login</h1>
