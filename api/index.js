14c1b0bbee7cd9b39b1912afcd3a496c4b68c885
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-11-27 15:36:05 +0000

    Checkin if Logged In

--- api/index.js
+++ api/index.js
@@ -5,12 +5,14 @@ const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const app = express();
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'jkjasdknkjnasdndoainkllsoijlanfkns';
 
-app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect('mongodb+srv://santiagoquirozdev:dNGo2RfuoYRP2Np4@cluster0.v0dd31q.mongodb.net/?retryWrites=true&w=majority');
 
@@ -41,6 +43,15 @@ app.post('/login', async (req, res) => {
     } else {
         res.status(400).json('Mail o contraseña incorrectos');
     }
+});
+
+app.get('/profile', (req, res) => {
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if(err) throw err;
+        res.json(info);
+    });
+    res.json(req.cookies);
 });
 
 app.listen(4000);
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,35 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+    const [username, setUsername] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:4000/profile', {
+            credentials: 'include',
+        }).then(response => {
+            response.json().then(userInfo => {
+                setUsername(userInfo.username);
+            });
+        });
+    }, [])
+
     return (
         <header>
             <Link to="/" className="logo">Blog</Link>
             <nav>
-                <Link to="/login">Login</Link>
-                <Link to="/register">Register</Link>
+                {username && (
+                    <>
+                        <Link to="/create">Crea un nuevo articulo</Link>
+                        <a href='/'>Logout</a>
+                    </>
+                )}
+                {!username && (
+                    <>
+                        <Link to="/login">Login</Link>
+                        <Link to="/register">Register</Link>
+                    </>
+                )}
             </nav>
         </header>
     )
