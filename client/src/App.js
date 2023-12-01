71c006f793f7581130057bcbbf4d10e81499ebea
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-12-01 09:29:59 +0000

    Single page post

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import { UserContextProvider } from './contexts/UserContext';
 import CreatePost from './pages/CreatePost';
+import PostPage from './pages/PostPage';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path={'/login'} element={<LoginPage />} />
           <Route path={'/register'} element={<RegisterPage />} />
           <Route path={'/create'} element={<CreatePost />} />
+          <Route path={'/post/:id'} element={<PostPage />} />
         </Route>
       </Routes>
     </UserContextProvider>
