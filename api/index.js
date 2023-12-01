f99d9963ddc1ddf07a4b3512f31fbc6beb0412da
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-12-01 00:41:37 +0000

    Create new post page

--- api/index.js
+++ api/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const { default: mongoose } = require('mongoose');
 const User = require('./models/User');
+const Post = require('./models/Post');
 const bcrypt = require('bcryptjs');
 const app = express();
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const uploadMiddleware = multer({ dest: 'uploads/' });
+const fs = require('fs');
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'jkjasdknkjnasdndoainkllsoijlanfkns';
@@ -60,5 +64,36 @@ app.get('/profile', (req, res) => {
 app.post('/logout', () => {
     res.cookie('token', '').json('ok');
 });
+
+app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
+    let newPath = null;
+    if (req.file) {
+      const {originalname,path} = req.file;
+      const parts = originalname.split('.');
+      const ext = parts[parts.length - 1];
+      newPath = path+'.'+ext;
+      fs.renameSync(path, newPath);
+    }
+  
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, async (err,info) => {
+      if (err) throw err;
+      const {id,title,summary,content} = req.body;
+      const postDoc = await Post.findById(id);
+      const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+      if (!isAuthor) {
+        return res.status(400).json('you are not the author');
+      }
+      await postDoc.update({
+        title,
+        summary,
+        content,
+        cover: newPath ? newPath : postDoc.cover,
+      });
+  
+      res.json(postDoc);
+    });
+  
+  });
 
 app.listen(4000);
--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import { UserContextProvider } from './contexts/UserContext';
+import CreatePost from './pages/CreatePost';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route index element={<IndexPage />} />
           <Route path={'/login'} element={<LoginPage />} />
           <Route path={'/register'} element={<RegisterPage />} />
+          <Route path={'/create'} element={<CreatePost />} />
         </Route>
       </Routes>
     </UserContextProvider>
