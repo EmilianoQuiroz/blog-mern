6290e38fcec7e9733bf9383bda3558d60c429b93
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-12-02 10:13:09 +0000

    User Context correction

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,8 +1,8 @@
 import { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { formatISO9075 } from 'date-fns';
-import { UserContext } from '../UserContext';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../contexts/UserContext';
 
 const PostPage = () => {
     const [postInfo, setPostInfo] = useState(null);
